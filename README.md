1ea6eb8443a0f24ec14eeadd653c3e289a9a2980
Prashant Varma <prashantvarma5083@gmail.com>
2024-02-13 22:57:38 +0530

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+## psmon 
+
+`psmon` is a alternaive of `nodemon`, which helps build Node.Js applications by automatically restarting the node application when file changes in the directory are detected.
+
---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+module.exports = require('./lib/psmon.js');
+
+  console.log("Hey! Welcome to psmon"); 
+
+
+
+// #! is called shebang. Its main purpose is to inform the system what type of script is included in the rest of the file. 
+
